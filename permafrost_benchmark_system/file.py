f2f9dba2824bc7816506c9206a52727bd2225478
Mark Piper <mark.piper@colorado.edu>
2017-08-04 13:58:06 -0600

    Add relationships to variables

--- permafrost_benchmark_system/file.py
+++ permafrost_benchmark_system/file.py
@@ -1,6 +1,7 @@
 """Perform file operations in PBS."""
 
 import os
+import yaml
 from ConfigParser import SafeConfigParser
 from . import data_directory
 
@@ -8,6 +9,8 @@ from . import data_directory
 class IlambConfigFile(object):
 
     """Tool for generating an ILAMB config file."""
+
+    sources_file = os.path.join(data_directory, 'cmip5-variables.yaml')
 
     def __init__(self,
                  variables=('gpp',),
@@ -23,8 +26,8 @@ class IlambConfigFile(object):
             CMIP5 short names (e.g., *gpp*, not *Gross Primary
             Production*).
         relationships : boolean, optional
-            Set to calculate dyadic relationships between the input
-            variables (default is False).
+            Set to calculate relationships between the input variables
+            (default is False).
         config_file : str, optional
             The name of the ILAMB config file (default is
             **ilamb.cfg**).
@@ -41,12 +44,18 @@ class IlambConfigFile(object):
             self.variables = variables,
         else:
             self.variables = variables
-        self.relationships = relationships
+        self.has_relationships = relationships
+        self.sources = None
         self.config_file = config_file
+
         self.config = dict()
         for var in self.variables:
             self.config[var] = SafeConfigParser()
             self.read(var)
+
+        if self.has_relationships and len(self.variables) > 1:
+            self.get_sources()
+            self.add_relationships()
 
     def get_template_file(self, var_name):
         """Gets path to a variable's template file.
@@ -70,6 +79,25 @@ class IlambConfigFile(object):
         """
         tmpl_file = self.get_template_file(var_name)
         self.config[var_name].read(tmpl_file)
+
+    def get_sources(self):
+        with open(self.sources_file, 'r') as fp:
+            self.sources = yaml.safe_load(fp)
+
+    def add_relationships(self):
+        for var in self.variables:
+            all_vars = list(self.variables)
+            all_vars.pop(all_vars.index(var))
+            relations = list()
+            for other in all_vars:
+                other_src = '/'.join([self.sources[other]['long_name'], self.sources[other]['benchmark_source']])
+                other_src = '"' + other_src + '"'
+                relations.append(other_src)
+            if len(all_vars) > 1:
+                rel_string = ','.join(relations)
+            else:
+                rel_string = relations[0]
+            self.config[var].set(self.sources[var]['benchmark_source'], 'relationships', rel_string)
 
     def write(self):
         """Writes an ILAMB config file."""
