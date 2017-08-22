b84a3a1abbca5e2b59afcd5672cd1e716ab4b3bf
Mark Piper <mark.piper@colorado.edu>
2017-08-22 11:25:07 -0600

    Add a sample netCDF regions file and unit tests

--- permafrost_benchmark_system/tests/test_file.py
+++ permafrost_benchmark_system/tests/test_file.py
@@ -2,13 +2,17 @@
 
 import os
 from nose.tools import raises, assert_true, assert_false, assert_equal
-from permafrost_benchmark_system.file import get_region_labels_txt
+from permafrost_benchmark_system.file import (get_region_labels_txt,
+                                              get_region_labels_ncdf)
 from permafrost_benchmark_system import data_directory
 
 
 regions_file_txt = 'tropics.txt'
 regions_file_txt_path = os.path.join(data_directory, regions_file_txt)
-labels = ['tropics', 'afritrop']
+labels_txt = ['tropics', 'afritrop']
+regions_file_nc = 'basins_0.5x0.5.nc'
+regions_file_nc_path = os.path.join(data_directory, regions_file_nc)
+labels_nc = ['Amazon', 'Ob', 'Lena']
 
 
 @raises(IOError)
@@ -23,4 +27,19 @@ def test_get_region_labels_txt_type():
 
 def test_get_region_labels_txt_value():
     x = get_region_labels_txt(regions_file_txt_path)
-    assert_equal(x, labels)
+    assert_equal(x, labels_txt)
+
+
+@raises(IOError)
+def test_get_region_labels_ncdf_path():
+    x = get_region_labels_ncdf(regions_file_nc)
+
+
+def test_get_region_labels_ncdf_type():
+    x = get_region_labels_ncdf(regions_file_nc_path)
+    assert_true(type(x), list)
+
+
+def test_get_region_labels_ncdf_value():
+    x = get_region_labels_ncdf(regions_file_nc_path)
+    assert_equal(x[0:3], labels_nc)
