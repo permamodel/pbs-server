7ed8840d5b95e79cc66bf8c165ea85767ee1cc5a
Mark Piper <mark.piper@colorado.edu>
2017-09-19 10:51:46 -0600

    Set up initial BMI for model and benchmark ingest tools

---
+++ permafrost_benchmark_system/bmi_ingest.py
@@ -0,0 +1,66 @@
+"""Define the BMI for the PBS ingest tools."""
+
+from basic_modeling_interface import Bmi
+from .ingest import ModelIngest, BenchmarkIngest
+
+
+class BmiIngestToolBase(Bmi):
+
+    _component_name = 'IngestToolBase'
+
+    def __init__(self):
+        self._config_file = None
+        self._tool = None
+        self._time = self.get_start_time()
+
+    def get_component_name(self):
+        return self._component_name
+
+    def initialize(self, filename):
+        self._config_file = filename
+
+    def update(self):
+        self._time = self.get_end_time()
+
+    def update_until(self, time):
+        self.update()
+
+    def finalize(self):
+        pass
+
+    def get_input_var_names(self):
+        return ()
+
+    def get_output_var_names(self):
+        return ()
+
+    def get_start_time(self):
+        return 0.0
+
+    def get_end_time(self):
+        return 1.0
+
+    def get_current_time(self):
+        return self._time
+
+    def get_time_step(self):
+        return 1.0
+
+    def get_time_units(self):
+        return 's'
+
+
+class BmiModelIngestTool(BmiIngestToolBase):
+
+    _component_name = 'ModelIngestTool'
+
+    def __init__(self):
+        self._tool = ModelIngest()
+
+
+class BmiBenchmarkIngestTool(BmiIngestToolBase):
+
+    _component_name = 'BenchmarkIngestTool'
+
+    def __init__(self):
+        self._tool = BenchmarkIngest()
