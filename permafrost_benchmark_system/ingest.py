aacc1c26eee21d9b2eab738851814f90fcca47db
Mark Piper <mark.piper@colorado.edu>
2017-10-20 16:36:22 -0600

    Complete init and load methods of ModelIngest class

--- permafrost_benchmark_system/ingest.py
+++ permafrost_benchmark_system/ingest.py
@@ -1,12 +1,27 @@
 """Perform ingest operations in PBS."""
+import yaml
+from .file import IngestFile
+
+
+models_dir = '/nas/data/tmp'
 
 
 class ModelIngest(object):
 
     """Operator for uploading model outputs into PBS."""
 
-    def __init__(self):
-        pass
+    def __init__(self, ingest_file=None):
+        self.ingest_files = []
+        self.make_public = True
+        if ingest_file is not None:
+            self.load(ingest_file)
+
+    def load(self, ingest_file):
+        with open(ingest_file, 'r') as fp:
+            cfg = yaml.safe_load(fp)
+        for f in cfg['ingest_files']:
+            self.ingest_files.append(IngestFile(f))
+        self.make_public = cfg['make_public']
 
 
 class BenchmarkIngest(object):
