c1935545a3ad89fdeda0317e1189b774a3fd0e63
Mark Piper <mark.piper@colorado.edu>
2017-10-31 14:27:34 -0600

    Add docstrings

--- permafrost_benchmark_system/ingest.py
+++ permafrost_benchmark_system/ingest.py
@@ -1,4 +1,5 @@
 """Perform ingest operations in PBS."""
+
 import os
 import shutil
 import yaml
@@ -6,6 +7,7 @@ from .file import IngestFile
 
 
 models_dir = '/nas/data/tmp'
+
 file_exists_msg = '''# File Exists\n
 The file `{1}/{0}` already exists in the PBS data store.
 The file has not been updated.
@@ -16,9 +18,22 @@ The file `{}` has been moved to `{}` in the PBS data store.
 
 
 class ModelIngest(object):
+    """
+    Operator for uploading model outputs into PBS.
 
-    """Operator for uploading model outputs into PBS."""
+    Parameters
+    ----------
+    ingest_file : str, optional
+      Path to the configuration file (default is None).
 
+    Attributes
+    ----------
+    ingest_files : list
+      List of files to ingest.
+    make_public : bool
+      Set to True to allow others to see and use ingested files.
+
+    """
     def __init__(self, ingest_file=None):
         self.ingest_files = []
         self.make_public = True
@@ -26,6 +41,15 @@ class ModelIngest(object):
             self.load(ingest_file)
 
     def load(self, ingest_file):
+        """
+        Read and parse the contents of a configuration file.
+
+        Parameters
+        ----------
+        ingest_file : str
+          Path to the configuration file.
+
+        """
         with open(ingest_file, 'r') as fp:
             cfg = yaml.safe_load(fp)
         for f in cfg['ingest_files']:
@@ -33,10 +57,24 @@ class ModelIngest(object):
         self.make_public = cfg['make_public']
 
     def validate(self):
+        """
+        Validate ingest files against the CMIP5 standard format.
+        """
         for f in self.ingest_files:
             f.is_valid = True
 
     def move(self):
+        """
+        Move ingest files to the ILAMB MODELS directory.
+
+        Notes
+        -----
+        A file that is moved is replaced with a text file listing the
+        new location of the file. If the file exists in the target
+        location, the file is replaced with a text file stating that
+        the file was not moved.
+
+        """
         for f in self.ingest_files:
             if f.is_valid:
                 msg = file_moved_msg.format(f.name, models_dir)
@@ -54,6 +92,7 @@ class ModelIngest(object):
 
     def cleanup(self):
         pass
+
 
 class BenchmarkIngest(object):
 
