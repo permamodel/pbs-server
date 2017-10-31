d47a0be0f22ae9f0a43e1a681e5e29dc1b40dac6
Mark Piper <mark.piper@colorado.edu>
2017-10-31 17:24:18 -0600

    Create unit tests for the ModelIngest class

---
+++ permafrost_benchmark_system/tests/test_modelingest.py
@@ -0,0 +1,86 @@
+"""Tests for the ModelIngest class."""
+
+import os
+import shutil
+import yaml
+from nose.tools import nottest, assert_true, assert_equal
+from permafrost_benchmark_system.ingest import ModelIngest
+
+
+ingest_file = 'test_ingest.yaml'
+model_file = 'test_model.txt'
+note_file = model_file + '.txt'
+tmp_dir = 'tmp'
+
+
+@nottest
+def make_test_files():
+    with open(model_file, 'w') as fp:
+        fp.write('This is a test model output file.\n')
+    cfg = {'ingest_files': [model_file], 'make_public': True}
+    with open(ingest_file, 'w') as fp:
+        yaml.safe_dump(cfg, fp, default_flow_style=False)
+
+
+def setup_module():
+    make_test_files()
+    os.mkdir(tmp_dir)
+
+
+def teardown_module():
+    shutil.rmtree(tmp_dir)
+    for f in [ingest_file, model_file, note_file]:
+        try:
+            os.remove(f)
+        except:
+            pass
+
+
+def test_init_no_parameters():
+    x = ModelIngest()
+    assert_true(isinstance(x, ModelIngest))
+
+
+def test_load():
+    x = ModelIngest()
+    x.load(ingest_file)
+    assert_equal(x.ingest_files[0].name, model_file)
+
+
+def test_init_one_parameter():
+    x = ModelIngest(ingest_file=ingest_file)
+    assert_equal(x.ingest_files[0].name, model_file)
+
+
+def test_validate():
+    pass
+
+
+def test_leave_file_note():
+    x = ModelIngest()
+    msg = 'Hi there'
+    x._leave_file_note(model_file, msg)
+    assert_true(os.path.isfile(note_file))
+
+
+def test_move_file_new():
+    x = ModelIngest()
+    x.load(ingest_file)
+    x.validate()
+    x.move(tmp_dir)
+    assert_true(os.path.isfile(os.path.join('tmp', model_file)))
+    assert_true(os.path.isfile(note_file))
+
+
+def test_move_file_exists():
+    make_test_files()
+    x = ModelIngest()
+    x.load(ingest_file)
+    x.validate()
+    x.move(tmp_dir)
+    assert_true(os.path.isfile(os.path.join('tmp', model_file)))
+    assert_true(os.path.isfile(note_file))
+
+
+def test_cleanup():
+    pass
