5f89f931ae97e40d61f330b80c513832640d12fe
Mark Piper <mark.piper@colorado.edu>
2017-11-07 11:01:07 -0700

    Create a logger for messages
    It takes messages in Markdown, converts them to HTML, and writes
    them to the file `index.html`. I considered jinja2 templates, but
    this was quick to set up.

--- permafrost_benchmark_system/file.py
+++ permafrost_benchmark_system/file.py
@@ -2,9 +2,24 @@
 
 import os
 import yaml
+import markdown
 from netCDF4 import Dataset
 from ConfigParser import SafeConfigParser
 from . import data_directory
+
+
+header = '''<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>PBS Ingest Tool Summary</title>
+</head>
+<body>
+'''
+footer = '''
+</body>
+</html>
+'''
 
 
 def get_region_labels_txt(regions_file):
@@ -210,3 +225,44 @@ class IngestFile(object):
         self.name = filename
         self.is_verified = False
         self.data = None
+
+
+class Logger(object):
+    """
+    A tool to generate an HTML log file from Markdown messages.
+
+    Parameters
+    ----------
+    title : str, optional
+      The title of the log file.
+
+    Attributes
+    ----------
+    data : str
+      The contents of the log.
+
+    """
+    def __init__(self, title='Summary'):
+        self.data = markdown.markdown('# {}'.format(title))
+
+    def add(self, message):
+        """
+        Add a message to the log.
+
+        Parameters
+        ----------
+        message : str
+          A message.
+
+        """
+        self.data += markdown.markdown(message)
+
+    def write(self):
+        """
+        Write the log file `index.html`.
+
+        """
+        with open('index.html', 'w') as fp:
+            fp.write(header)
+            fp.write(self.data)
+            fp.write(footer)
