6574b4c1a8ae7dfc48b04f335b3dbdc9532533ba
Mark Piper <mark.piper@colorado.edu>
2017-11-06 19:08:01 -0700

    Add docstrings

--- permafrost_benchmark_system/verify.py
+++ permafrost_benchmark_system/verify.py
@@ -4,7 +4,20 @@ from netCDF4 import Dataset
 
 
 class VerificationError(Exception):
+    """
+    Raise this exception when a file doesn't pass a verification test.
 
+    Parameters
+    ----------
+    msg : str
+      A human-readable message.
+
+    Attributes
+    ----------
+    msg : str
+      A human-readable message.
+
+    """
     def __init__(self, msg):
         self.msg = ': '.join([type(self).__name__, msg])
 
@@ -16,17 +29,43 @@ class VerificationTool(object):
     """
     Tool for verifying that files are CMIP5-compatible.
 
+    Parameters
+    ----------
+    file : str
+      The name of a file to verify.
+
+    Attributes
+    ----------
+    file : str
+      The name of a file to verify.
+    parts : list
+      Parts of the filename (see Notes below).
+    variable_name : str or None
+      CMIP5 short variable name.
+    mip_table : str or None
+      MIP table name.
+    model_name : str or None
+      Model name.
+    experiment : str or None
+      Experiment type.
+    ensemble_member : str or None
+      CMIP5 ensemble member.
+    temporal_subset : str or None
+      Time period covered by model
+
     Notes
     -----
-    filename = <variable name>_<MIP table>_<model>_<experiment>_<ensemble
-member>[_<temporal subset>].nc
+    A CMIP5-compatible file name takes the following form:
+
+        filename = <variable name>_<MIP table>_<model>_<experiment>_<ensemble_member>[_<temporal subset>].nc
 
     An example:
-      tas_Amon_HADCM3_ historical_r1i1p1_185001-200512.nc
+
+        tas_Amon_HADCM3_ historical_r1i1p1_185001-200512.nc
 
     """
-    def __init__(self, ingest_file):
-        self.file = ingest_file
+    def __init__(self, file):
+        self.file = file
         self.parts = []
         self.variable_name = None
         self.mip_table = None
@@ -36,21 +75,37 @@ member>[_<temporal subset>].nc
         self.temporal_subset = None
 
     def is_netcdf(self):
+        """
+        Check whether a file is netCDF.
+
+        """
         try:
             Dataset(self.file.name)
         except IOError as e:
             raise VerificationError(e.message)
 
     def is_netcdf3_data_model(self):
+        """
+        Check whether a netCDF file uses the classic data model.
+
+        """
         d = Dataset(self.file.name)
         if not d.data_model in ['NETCDF3_CLASSIC', 'NETCDF4_CLASSIC']:
             msg = 'NetCDF: File must use classic data model'
             raise VerificationError(msg)
 
     def parse_filename(self):
+        """
+        Break a filename into its component parts.
+
+        """
         self.parts = self.file.name.split('_')
 
     def filename_has_model_name(self):
+        """
+        Check that the filename includes a model name.
+
+        """
         try:
             self.model_name = self.parts[2]
         except IndexError:
@@ -58,6 +113,12 @@ member>[_<temporal subset>].nc
             raise VerificationError(msg)
 
     def verify(self):
+        """
+        Run all checks.
+
+        A file that passes all checks is verified.
+
+        """
         self.is_netcdf()
         self.is_netcdf3_data_model()
         self.parse_filename()
