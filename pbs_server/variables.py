70334d710e6b4b8a4007d63bfbfe49e51be575dc
Mark Piper <mark.piper@colorado.edu>
2018-07-13 13:43:03 -0600

    Fix issue with get_variable_name

--- pbs_server/variables.py
+++ pbs_server/variables.py
@@ -1,5 +1,8 @@
 """Perform operations on benchmark data uploaded through PBS."""
 
+import os
+
+
 def get_variable_name(pbs_file):
     """
     Extract the CMIP5 short variable name from a benchmark data filename.
@@ -15,6 +18,7 @@ def get_variable_name(pbs_file):
       The name of the variable.
 
     """
-    parts = pbs_file.split('_')
+    base, ext = os.path.splitext(pbs_file)
+    parts = base.split('_')
     name = parts[0]
     return name
