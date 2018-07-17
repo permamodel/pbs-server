4ea8d62bda97a3c700cd92c073ad81ad74b00aa8
Mark Piper <mark.piper@colorado.edu>
2018-07-17 16:34:10 -0600

    Add update_template function to variables module

--- pbs_server/tests/test_variables.py
+++ pbs_server/tests/test_variables.py
@@ -1,21 +1,38 @@
 """Tests for the variables module."""
 
 import os
+import shutil
 import json
+from ConfigParser import SafeConfigParser
 from nose.tools import assert_equal, assert_true, assert_false
-from pbs_server.variables import get_name, update_parameters
+from pbs_server.variables import (get_name, update_parameters,
+                                  update_template)
 from pbs_server import data_directory
 
 
 variable_file = 'gpp_0.5x0.5.nc'
 variable_name = 'gpp'
-existing_var = 'tas'
-new_var = 'nep'
-variables = [existing_var, new_var]
+old_var = 'tas'
+new_var = 'foo'
+variables = [old_var, new_var]
+old_tmpl_file = os.path.join(data_directory,
+                             '{}.cfg.tmpl'.format(old_var))
+old_tmpl_file_copy = '{}.copy'.format(old_tmpl_file)
+new_tmpl_file = os.path.join(data_directory,
+                             '{}.cfg.tmpl'.format(new_var))
 
 parameters_file = os.path.join(data_directory, 'parameters.json')
 with open(parameters_file, 'r') as fp:
     parameters = json.load(fp)
+
+
+def setup_module():
+    shutil.copy(old_tmpl_file, old_tmpl_file_copy)
+
+
+def teardown_module():
+    shutil.move(old_tmpl_file_copy, old_tmpl_file)
+    os.remove(new_tmpl_file)
 
 
 def test_get_name():
@@ -33,4 +50,18 @@ def test_update_parameters():
     update_parameters(parameters, variables)
     for param in parameters:
         if param['key'] == '_variable1':
-            assert_true(existing_var in param['value']['choices'])
+            assert_true(old_var in param['value']['choices'])
+
+
+def test_create_template():
+    update_template(new_var, '{}.nc'.format(new_var))
+    config = SafeConfigParser()
+    config.read(new_tmpl_file)
+    assert_true(config.has_section('PBS'))
+
+
+def test_update_template():
+    update_template(old_var, '{}.nc'.format(old_var))
+    config = SafeConfigParser()
+    config.read(old_tmpl_file)
+    assert_true(config.has_section('PBS'))
--- pbs_server/variables.py
+++ pbs_server/variables.py
@@ -2,6 +2,11 @@
 
 import os
 import bisect
+from ConfigParser import SafeConfigParser
+from . import data_directory
+
+
+pbs_data_dir = os.path.join('DATA-by-project', 'PBS')
 
 
 def get_name(pbs_file):
@@ -48,3 +53,40 @@ def update_parameters(parameters, variables):
                 for variable in variables:
                     if variable not in param['value']['choices']:
                         bisect.insort(param['value']['choices'], variable)
+
+
+def update_template(variable_name, file_name):
+    """Create or update the .cfg.tmpl file for a variable.
+
+    Parameters
+    ----------
+    variable_name : str
+      A CMIP5 short variable name.
+    file_name : str
+      The path to the file on the PBS server.
+
+    """
+    base_file = variable_name + '.cfg.tmpl'
+    tmpl_file = os.path.join(data_directory, base_file)
+    tmpl_file_exists = os.path.exists(tmpl_file)
+    config_update = SafeConfigParser()
+
+    # If the file is new, add an [h2] entry.
+    if not tmpl_file_exists:
+        h2_section = 'h2: {}'.format(variable_name)
+        config_update.add_section(h2_section)
+        config_update.set(h2_section, 'variable', '"{}"'.format(variable_name))
+
+    config_existing = SafeConfigParser()
+    if tmpl_file_exists:
+        config_existing.read(tmpl_file)
+
+    # If a [PBS] entry doesn't exist, make it.
+    pbs_section = 'PBS'
+    if not config_existing.has_section(pbs_section):
+        config_update.add_section(pbs_section)
+        config_update.set(pbs_section, 'source',
+                          '"{}"'.format(os.path.join(pbs_data_dir, file_name)))
+
+    with open(tmpl_file, 'a') as fp:
+        config_update.write(fp)
